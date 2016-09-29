9f8a6604dc08a9ac1f82aca09ebd67f1b218e7f4
Mark Crossen <markcrossen@studentbody.byu.edu>
2016-09-29 01:08:48 -0600

    more comments

--- lib/diffie-hellman.rb
+++ lib/diffie-hellman.rb
@@ -2,8 +2,8 @@ require "openssl/bn"
 require "modularexponent"
 require "securerandom"
 
-KEY_SIZE = 500
-G = 5
+KEY_SIZE = 500 # default size of the prime modulus and the private key
+G = 5 # default generator
 
 class DiffieHellman
   attr_reader :p, :pub_key
@@ -13,10 +13,14 @@ class DiffieHellman
     other.modexp(@priv_key, @p)
   end
 
+  # create all the starting values
   def initialize(key_size=KEY_SIZE, generator=G)
     raise "key size must be divisible by 4" if key_size % 4 != 0
+    # generate a large prime with the openssl library
     @p = OpenSSL::BN::generate_prime(key_size, true).to_i
+    # the SecureRandom library uses urandom
     @priv_key = SecureRandom.random_number(2 ** key_size)
+    # calculate the public key based off the private key and p
     @pub_key = generator.modexp(@priv_key, @p)
   end
 
--- test/test_dh.rb
+++ test/test_dh.rb
@@ -2,14 +2,17 @@ require "minitest/autorun"
 require "diffie-hellman"
 require "openssl"
 
+# used to test if ints are prime or not
 module PrimeHelper
   def prime?
+    # convert to the openssl bignum and use that method
     OpenSSL::BN.new(self.to_s).prime?
   end
 end
 Fixnum.send(:include, PrimeHelper)
 Bignum.send(:include, PrimeHelper)
 
+# gives the private key a public accessor for the tests
 module DHhelper
   def priv_key
     @priv_key
@@ -17,8 +20,8 @@ module DHhelper
 end
 DiffieHellman.send(:include, DHhelper)
 
+# unit test for DiffieHellman
 class TestDiffieHellman < MiniTest::Test
-
   def test_p
     dh = DiffieHellman.new
     p = dh.p
@@ -29,14 +32,14 @@ class TestDiffieHellman < MiniTest::Test
 
   def test_pubkey
     dh = DiffieHellman.new
-    # use different modular exponentiation library for test
+    # use different library to compare against for test
     assert_equal(5.to_bn.mod_exp(dh.priv_key, dh.p).to_i, dh.pub_key)
   end
 
   def test_compute_key
     dh = DiffieHellman.new
     other_priv_key = 1998286638065473057944506344030256054916203227381748916180906390214373930105605405985818224246280726328877245115163209963634633681313092395058312190549
-    # use different modular exponentiation library for test
+    # use different library to compare against for test
     other_pub_key = 5.to_bn.mod_exp(other_priv_key, dh.p)
     other_secret = dh.pub_key.to_bn.mod_exp(other_priv_key, dh.p).to_i
     secret = dh.compute_key(other_pub_key.to_s)
